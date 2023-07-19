61795b534bd624a68aed92716c228324f6944647
kunnu223 <123104990+kunnu223@users.noreply.github.com>
2023-07-20 00:24:44 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://kunalkumawat223:kunalkumawat223@cluster0.5gmx8jg.mongodb.net/ecomm');
+mongoose.connect('mongodb+srv://kunalkumawat223:kunalkumawat223@cluster0.5gmx8jg.mongodb.net/');
